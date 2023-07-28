c90d7311132a3c6c3ee4265c147628213af7a83c
r0zymnuk <maksr240805@gmail.com>
2023-07-28 16:03:53 +0300

    Created Api for testing the application

---
+++ EventHub.Api/Controllers/EventController.cs
@@ -0,0 +1,33 @@
+﻿using EventHub.Application.Dtos.Request;
+using EventHub.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventHub.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class EventController : ControllerBase
+{
+    private readonly IEventService eventService;
+
+    public EventController(IEventService eventService)
+    {
+        this.eventService = eventService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllEvents()
+    {
+        var events = await eventService.GetAllEventsAsync();
+
+        return Ok(events);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateEvent([FromBody] AddEventDto addEvent)
+    {
+        var newEvent = await eventService.AddEvent(addEvent);
+
+        return CreatedAtAction(nameof(CreateEvent), new { id = newEvent.Id }, newEvent);
+    }
+}
