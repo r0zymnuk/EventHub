0499b525d25519f1f00a761239b2f3961ab69005
r0zymnuk <maksr240805@gmail.com>
2023-09-13 01:19:33 +0300

    Fixed one of the issues of event image urls

--- EventHub.Application/AutoMapper.cs
+++ EventHub.Application/AutoMapper.cs
@@ -3,6 +3,7 @@ using EventHub.Application.Dtos.Response;
 using EventHub.Application.Dtos.Response.Account.User;
 using EventHub.Application.Dtos.Response.Event;
 using EventHub.Domain.Entities;
+using Microsoft.Extensions.Logging;
 
 namespace EventHub.Application;
 public class AutoMapper : Profile
@@ -11,26 +12,28 @@ public class AutoMapper : Profile
     {
         CreateMap<Event, GetEventDto>();
         CreateMap<Event, EventModel>()
-        .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => string.IsNullOrWhiteSpace(src.ImageUrl) ? "https://via.placeholder.com/600x400?text=No%20Image" : src.ImageUrl));
+        .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => GetImageUrl(src.ImageUrl)));
         CreateMap<Event, EventCardModel>()
         .ConvertUsing(src => CardFromEvent(src));
-
         CreateMap<RegisterUserModel, User>();
         CreateMap<User, UserViewModel>();
     }
-
     public static EventCardModel CardFromEvent(Event @event)
     {
         var description = @event.Description.Length > 150 ? @event.Description[..150] + "..." : @event.Description;
-        var imageUrl = string.IsNullOrWhiteSpace(@event.ImageUrl) ? "https://via.placeholder.com/600x400?text=No%20Image" : @event.ImageUrl;
-
+        var imageUrl = GetImageUrl(@event.ImageUrl);
         return new EventCardModel(
-            @event.Id,
-            @event.Title,
-            description,
-            imageUrl,
+        @event.Id,
+        @event.Title,
+        description,
+        imageUrl,
             @event.Start,
             @event.Location.ToString()
         );
+    }
+
+    public static string GetImageUrl(string imageUrl)
+    {
+        return string.IsNullOrWhiteSpace(imageUrl) ? "https://via.placeholder.com/600x400?text=No%20Image" : (imageUrl.Contains(':') ? imageUrl : $"/image/get/{imageUrl}");
     }
 }
